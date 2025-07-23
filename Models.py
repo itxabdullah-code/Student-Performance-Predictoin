f8613d6d98fdf5c0761349b2914e80652db0b2e0
Muhammad Abdullah <abdullahmughal9379@gmail.com>
2025-07-23 20:26:43 +0500

    Update Models.py

--- Models.py
+++ Models.py
@@ -15,7 +15,7 @@ st.set_page_config(page_title="Student Performance Predictor", page_icon="🎓",
 st.markdown("""
     <style>
     .stApp {
-        background-color: #E9E3DF;
+        background-color: #273F4F;
         color: #000000;
     }
 
